c22ec50998a01d18f0232d25a0e1b1f8f31e4af0
Tharki-God <alokender018@gmail.com>
2024-04-26 23:55:21 +0530

    lint

--- src/lib/requiredModules.ts
+++ src/lib/requiredModules.ts
@@ -1,13 +1,13 @@
 import { webpack } from "replugged";
 import Types from "../types";
 
-export const Modules: Types.Modules = {}
+export const Modules: Types.Modules = {};
 
-Modules.loadModules = async (): Promise<void> =>{
-  Modules.Avatar ??= await webpack.waitForModule<Types.Avatar>(webpack.filters.bySource("getMaskId()"));
+Modules.loadModules = async (): Promise<void> => {
+  Modules.Avatar ??= await webpack.waitForModule<Types.Avatar>(
+    webpack.filters.bySource("getMaskId()"),
+  );
   Modules.MaskManager ??= await webpack.waitForProps<Types.MaskLibrary>("MaskLibrary");
-  
-}
+};
 
 export default Modules;
-
